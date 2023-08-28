da4d859b70b792a9657e4394a9d0f661b66be725
raikumar-lamzing <raikumar.khangembam@lamzing.com>
2023-08-28 14:57:41 +0530

    new layout added

--- resources/views/students.blade.php
+++ resources/views/students.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <title>Students Example Laravel</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{ asset('css/bsstyle.css') }}">
-</head>
-
-<body>
-    <div class="container mt-5">
+@extends('layouts.main')
+@section('title', 'Home Page')
+@section('master')
+   <div class="container mt-5">
         <h2>Student List</h2>
         <a href="/addstudent" type="button" class="btn btn-primary btn-sm">Add New Student</a>
         <table class="table table-striped mt-4">
@@ -22,14 +15,15 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($list as $student)
+               
+                @foreach ($list as $ss)
                     <tr>
-                        <td>{{ $student->name }}</td>
-                        <td>{{ $student->department }}</td>
-                        <td>{{ $student->email }}</td>
-                        <td><a href="/edit-student/{{$student->id}}" type="button" class="btn btn-info btn-sm">Update</a> 
+                        <td>{{ $ss->name }}</td>
+                        <td>{{ $ss->department }}</td>
+                        <td>{{ $ss->email }}</td>
+                        <td><a href="/edit-student/{{$ss->id}}" type="button" class="btn btn-info btn-sm">Update</a> 
                             
-                            <a href="/remove-student/{{$student->id}}" type="button"
+                            <a href="/remove-student/{{$ss->id}}" type="button"
                                 class="btn btn-danger btn-sm">Delete</a></td>
                     </tr>
                 @endforeach
@@ -38,6 +32,4 @@
         </table>
     </div>
 
-</body>
-
-</html>
+@endsection
